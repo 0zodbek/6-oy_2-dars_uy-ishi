6840af1d5133644fc23fc7d89f346e65f818541c
0zodbek <ozpdbekov@gmail.com>
2024-07-17 03:24:49 +0500

    l

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import './assets/data.json'
+import Card from './assets/components/Card'
+function App() {
+
+  return (
+  
+    <>
+    <Card></Card>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/assets/components/Card/index.jsx
@@ -0,0 +1,24 @@
+import "./index.css"
+function Card(){
+
+return (
+    <div className="card">
+
+            <img src="https://picsum.photos/200?random=1" alt="bu yerda rasm" />
+
+            <div className="part_info">
+
+                <h2>Valentin Satterfield</h2>
+
+                <div className="phone_email_wrapper"><h3>tell:(922) 346-6889  email:Therese.Boehm3@gmail.com</h3></div>
+
+                <h3>region: West Virginia, zip: 02291-8777</h3>
+
+                <h3>Toyota Toyota</h3>
+
+            </div>
+
+        </div>
+    )
+
+}
